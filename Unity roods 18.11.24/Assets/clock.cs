a31af622f883caa1ab2b9c6a01c791a2b70c075a
rodos1210 <rodion_nabokih@mail.ru>
2024-11-25 16:26:48 +0300

    lesson.25.11.24
    сделали секундную стрелку и плавное движение

--- Unity roods 18.11.24/Assets/clock.cs
+++ Unity roods 18.11.24/Assets/clock.cs
@@ -16,8 +16,8 @@ public class clock : MonoBehaviour
     private void Update()
     {
         DateTime t = DateTime.Now;
-        hoursarmpivot.localRotation = Quaternion.Euler(0, 0,-30 * t.Hour);
-        minutesarmpivot.localRotation = Quaternion.Euler(0, 0, -6 * t.Minute);
-        secondspivot.localRotation = Quaternion.Euler(0,0, -6 * t.Second);
+        hoursarmpivot.localRotation = Quaternion.Euler(0, 0,-30 * t.Hour - (0.5f * t.Minute));
+        minutesarmpivot.localRotation = Quaternion.Euler(0, 0, -6 * t.Minute - (0.1f *t.Second));
+        secondspivot.localRotation = Quaternion.Euler(0,0, -6 * t.Second -  (0.006f * t.Millisecond));
     }
 }
